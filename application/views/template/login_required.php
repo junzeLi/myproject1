b3f0f34a83e3434438e7c74b106cf0cb6c830043
junzeLi <junze.li206@gmail.com>
2021-10-22 01:30:12 +1000

    project

--- application/views/template/login_required.php
+++
@@ -1,8 +0,0 @@
-<div class="container d-flex flex-row justify-content-around align-items-center align-content-center login-notice">
-    <ul class="navbar-nav my-lg-0 d-flex d-xl-flex flex-column align-items-xl-center justify-content-center align-content-center">
-        <p> Can't wait to see your sharing! One more step before you continue! </p>
-            <li class="log-user">
-                <a href="<?php echo base_url(); ?>login" class="btn btn-light action-button" role="button"> Login </a>
-            </li>
-    </ul>
-</div>
